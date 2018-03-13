d11824eb2f4fa1b416cefb1786426f0809da805f
Christina Laba <christinalaba.m@gmail.com>
2018-03-13 01:01:01 -0400

    smooth scroll

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,32 @@
-const ohHey = "Hello World";
+const portfolio = {};
 
-console.log(ohHey);
+
+portfolio.scroll = function () {
+    $(function () {
+        $('a').smoothScroll({
+            offset: 0,
+            speed: 600
+        });
+    });
+}
+
+portfolio.init = () => {
+    portfolio.scroll();
+}
+
+
+$(function(){
+    portfolio.init();
+})
+
+
+
+// netflixChow.flickityMovie = function () {
+//     $('.movie-results').flickity({
+//         cellAlign: 'left',
+//         contain: true,
+//         pageDots: false,
+//         wrapAround: true
+//     });
+
+// }
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,30 @@
-"use strict";
+'use strict';
 
-var ohHey = "Hello World";
+var portfolio = {};
 
-console.log(ohHey);
+portfolio.scroll = function () {
+    $(function () {
+        $('a').smoothScroll({
+            offset: 0,
+            speed: 600
+        });
+    });
+};
+
+portfolio.init = function () {
+    portfolio.scroll();
+};
+
+$(function () {
+    portfolio.init();
+});
+
+// netflixChow.flickityMovie = function () {
+//     $('.movie-results').flickity({
+//         cellAlign: 'left',
+//         contain: true,
+//         pageDots: false,
+//         wrapAround: true
+//     });
+
+// }
